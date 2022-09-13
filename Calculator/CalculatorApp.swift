efd604b2e3c90da5d6f3d18cecff66f687f5c57d
Alexandru Ivanescu <alexbogdnivanescu@gmai.com>
2022-09-13 14:00:11 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alex Ivanescu on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
